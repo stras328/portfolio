1a0b38864935bbbe4c8c4b5b90ab537e17f2c5a6
sjhstrange <tacocatcupcake@gmail.com>
2025-10-15 18:34:10 -0400

    joKSJ:ODFIJSJI:FIJ:SD
    SHFCSDFSDJFJ::IJSDFJN

--- a-city-wrought-in-iron/index_script.js
+++ a-city-wrought-in-iron/index_script.js
@@ -1,23 +1,18 @@
-const images = [
-  'title cards/title card 2.jpg',
-  'title cards/title card 3.jpg',
-  'title cards/title card 5.jpg',
-  'title cards/title card 6.jpg',
-  'title cards/title card 7.jpg',
-  'title cards/title card 8.jpg',
-  'title cards/title card 9.jpg',
-  'title cards/title card 11.jpg',
-  'title cards/title card 12.jpg',
-  'title cards/title card 13.jpg',
-  'title cards/title card 14.jpg',
-  'title cards/title card 15.jpg',
-  'title cards/title card 16.jpg',
-];
-
 document.addEventListener("DOMContentLoaded", () => {
   const images = [
-    'title cards/title card 1.jpg',
-    // ...
+    'Title Cards/title card 2.JPG',
+    'Title Cards/title card 3.JPG',
+    'Title Cards/title card 5.JPG',
+    'Title Cards/title card 6.JPG',
+    'Title Cards/title card 7.JPG',
+    'Title Cards/title card 8.JPG',
+    'Title Cards/title card 9.JPG',
+    'Title Cards/title card 11.JPG',
+    'Title Cards/title card 12.JPG',
+    'Title Cards/title card 13.JPG',
+    'Title Cards/title card 14.JPG',
+    'Title Cards/title card 15.JPG',
+    'Title Cards/title card 16.JPG',
   ];
 
   let index = 0;
@@ -25,10 +20,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   setInterval(() => {
     img.style.opacity = 0;
+
     setTimeout(() => {
       index = (index + 1) % images.length;
       img.src = images[index];
       img.style.opacity = 1;
-    }, 500); // match CSS transition duration (0.5s = 500ms)
+    }, 500); // match CSS fade duration
   }, 1250);
 });
